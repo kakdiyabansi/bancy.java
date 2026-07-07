1e1cca2e9f3d8253e713ca872a6acf301d97758a
kakdiyabansi <kakdiyabansi13@gmail.com>
2026-07-07 09:11:26 +0530

    Add files via upload

---
+++ foreach.java
@@ -0,0 +1,19 @@
+class foreach
+ {
+	 public static void main (String args[])
+	 {
+		 int numbers[]={10,20,30,40,50};
+		 for (int x:numbers)
+		 {
+			 System.out.print(x+",");
+		 }
+         System.out.println();
+
+         String names[]={"james","larry","tom","lacy"};
+
+        for(String name:names)		   
+	    {
+				System.out.print(name+",");
+	    }
+	 }
+ }	 
