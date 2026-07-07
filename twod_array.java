e247defeeef86300aa7f22175c51d7a7e5c5d49c
kakdiyabansi <kakdiyabansi13@gmail.com>
2026-07-07 09:12:33 +0530

    Add files via upload

---
+++ twod_array.java
@@ -0,0 +1,15 @@
+class twod_array
+{
+	public static void main (String args[])
+	{
+		int a[][]={{99,40,34},{76,55,67},{89,58,45}};
+		for(int i=0;i<a.length;i++)
+		{
+			for(int i=0;j<a.length;j++)
+		    {
+			 System.out.print(a[i][j]+"\t");
+		    }
+		 System.out.println();
+		}
+	}
+}	
